ac87b37b4b9dc2c4a1b968ab06a85b0bd3b8f890
zczerska <zofia.hello@gmail.com>
2020-05-28 19:53:44 -0400

    js tag fixed

--- JS/chartjs2.js
+++ JS/chartjs2.js
@@ -2,10 +2,10 @@ var ctx = document.getElementById('myChart2');
 var myChart2 = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['0', '1', '5', '7', '6'],
+        labels: ['1', '2', '3', '4', '5'],
         datasets: [{
             label: 'comfort level: least(1) to most(5)',
-            data: [2, 5, 2, 7, 3],
+            data: [0, 1, 5, 7, 6],
             backgroundColor: [
                 'rgba(75, 192, 192, 0.2)',
 				'rgba(75, 192, 192, 0.2)',
